450c0163c015641e044e663daa8124d34ffffdba
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2025-01-03 15:45:17 +0100

    Fix auth verification

--- src/Pages/Agent/Callback.js
+++ src/Pages/Agent/Callback.js
@@ -1,6 +1,23 @@
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
+const fetchData = async (oauthToken, oauthVerifier) => {
+    const response = await fetch(`https://pumpkinai.icademics.com/auth/callback?oauth_token=${oauthToken}&oauth_verifier=${oauthVerifier}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+  
+      if (!response.ok) {
+        throw new Error('Failed to initialise Twitter Login');
+      }
+  
+    const result = await response.json();
+    return result.data;
+};
+
 const Loading = () => {
     const location = useLocation();
 
@@ -10,8 +27,16 @@ const Loading = () => {
         const oauthVerifier = params.get('oauth_verifier');
 
         if (oauthToken && oauthVerifier) {
-            const callbackUrl = `https://pumpkinai.icademics.com/auth/callback?oauth_token=${oauthToken}&oauth_verifier=${oauthVerifier}`;
-            window.location.href = callbackUrl;
+            fetchData(oauthToken, oauthVerifier)
+            .then((data) => {
+                const { access_token, user } = data;
+                localStorage.setItem('access_token', access_token);
+                localStorage.setItem('user', JSON.stringify(user));
+                window.location.href = '/agent';
+            })
+            .catch((error) => {
+                console.error(error);
+            });
         }
     }, [location]);
 
